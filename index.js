6c8555404ba0b5464a3812de2775ec76d570d3bd
Eman Gurung <egurung1117@gmail.com>
2023-08-07 01:43:07 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  const para = document.querySelector("#text");
+  para.textContent = "This is really cool!";
+});
